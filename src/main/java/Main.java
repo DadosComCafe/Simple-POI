60d30a4182260d635503a79c32b3a0dd7fe104bd
Victor Santos Silva <dadoscomcafe.dev@gmail.com>
2026-07-21 23:06:35 -0300

    (src/): Modify println

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,10 @@
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+import java.io.File;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("It is your first code in Java!");
+    public static void main(String[] args) throws Exception {
+        var workbook = WorkbookFactory.create(new File("src/main/resources/data/sample1.xlsx"));
+        System.out.println("Este arquivo possui " + workbook.getNumberOfSheets() + " planilhas.");
+        workbook.close();
     }
 }
