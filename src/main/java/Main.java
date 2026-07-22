be0182a04a4b1addcd39b09dc9b91f15ab66b2d7
Victor Santos Silva <dadoscomcafe.dev@gmail.com>
2026-07-21 23:33:43 -0300

    (src/main/java/Main.java): Add a bit more complexity to the hello world java poi code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,17 @@ import java.io.File;
 public class Main {
     public static void main(String[] args) throws Exception {
         var workbook = WorkbookFactory.create(new File("src/main/resources/data/sample1.xlsx"));
-        System.out.println("Este arquivo possui " + workbook.getNumberOfSheets() + " planilhas.");
+        int numeroDePlanilhas = workbook.getNumberOfSheets();
+        System.out.println("Este arquivo possui " + numeroDePlanilhas + " planilhas.");
+        if (numeroDePlanilhas > 1){
+            System.out.println("O nome das planilhas são listados a seguir: ");
+            for (int i = 0; i < numeroDePlanilhas; i ++){
+                System.out.println("A planilha " + i + " possui o nome: " + workbook.getSheetName(i));
+            }
+        }
+        else {
+            System.out.println("O nome da planilha é: " + workbook.getSheetName(0) + ".");
+        }
         workbook.close();
     }
 }
