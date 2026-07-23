5d7bbd55cfbb8b8cc7ec82fa18c2e9a151b8773d
Victor Santos Silva <dadoscomcafe.dev@gmail.com>
2026-07-22 21:16:35 -0300

    (simplePoi/excel/ExcelFile.java): Create a class and use it in main

--- src/main/java/br/com/simplePoi/excel/ExcelFile.java
+++ src/main/java/br/com/simplePoi/excel/ExcelFile.java
@@ -1,4 +1,38 @@
 package br.com.simplePoi.excel;
 
-public class ExcelFile {
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExcelFile implements AutoCloseable {
+    private final File file;
+    private final Workbook workbook;
+
+    public ExcelFile(String path) throws IOException {
+        this.file = new File(path);
+        this.workbook= WorkbookFactory.create(file);
+    }
+
+    public Workbook getWorkbook(){
+        return workbook;
+    }
+
+    public int getNumeroDePlanilhas(){
+        return workbook.getNumberOfSheets();
+    }
+
+    public String getNomePlanilha(int index){
+        return workbook.getSheetName(index);
+    }
+
+    public File getFile(){
+        return file;
+    }
+
+    @Override
+    public void close() throws Exception{
+        workbook.close();
+    }
 }
