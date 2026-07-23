db361df5c540bc27f380cd5e6904ce7f886faa9f
Victor Santos Silva <dadoscomcafe.dev@gmail.com>
2026-07-22 21:18:06 -0300

    (Main.java): Use the utils class in the Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,22 @@
-import org.apache.poi.ss.usermodel.WorkbookFactory;
-import java.io.File;
+package br.com.simplePoi;
+
+import br.com.simplePoi.excel.ExcelFile;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        var workbook = WorkbookFactory.create(new File("src/main/resources/data/sample1.xlsx"));
-        int numeroDePlanilhas = workbook.getNumberOfSheets();
-        System.out.println("Este arquivo possui " + numeroDePlanilhas + " planilhas.");
-        if (numeroDePlanilhas > 1){
-            System.out.println("O nome das planilhas são listados a seguir: ");
-            for (int i = 0; i < numeroDePlanilhas; i ++){
-                System.out.println("A planilha " + i + " possui o nome: " + workbook.getSheetName(i));
+    public static void main(String[] args) {
+        try (ExcelFile excel = new ExcelFile("src/main/resources/data/sample1.xlsx")) {
+            System.out.println("Este arquivo possui " + excel.getNumeroDePlanilhas() + " planilhas.");
+            for (int i = 0; i < excel.getNumeroDePlanilhas(); i++) {
+                System.out.println("A planilha " + i + " possui o nome: " + excel.getNomePlanilha(i));
             }
+
+            tratarArquivo(excel);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        else {
-            System.out.println("O nome da planilha é: " + workbook.getSheetName(0) + ".");
-        }
-        workbook.close();
+    }
+
+    public static void tratarArquivo(ExcelFile excel) {
+        System.out.println("Tratando arquivo: " + excel.getFile().getName());
     }
 }
